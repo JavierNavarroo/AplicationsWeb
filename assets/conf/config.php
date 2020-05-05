2641366ed9c704a88d8b1ed75f10f0a17bf19e26
kikito98 <63065119+kikito98@users.noreply.github.com>
2020-05-05 10:46:59 +0200

    vuelos oo

--- assets/conf/config.php
+++ assets/conf/config.php
@@ -1,10 +1,6 @@
 <?php
 
-<<<<<<< HEAD
 require_once __DIR__.'/Aplicacion.php';
-=======
-require_once '../iVELiTE/assets/conf/Aplicacion.php';
->>>>>>> master
 
 /**
  * Parámetros de conexión a la BD
--- index.php
+++ index.php
@@ -52,7 +52,7 @@
 					
 					<div class="container">
 					<div class="newbox"> 	<!-- LA CLASE NEWBOX ES CREADA DE CERO -->
-						<form method="post" action="#">
+						<form method="post" action="buscar.php">
 							<div class="row gtr-50 gtr-uniform">
 								<div class="col-6 col-12-mobilep">
 									<input type="textbuscador" name="origen" id="origen" value="" placeholder="Origen" />
@@ -63,8 +63,8 @@
 								<div class="col-12" >
 									<div class="centro">
 										<p type="text">Elije la fecha de inicio y fin: 
-											<input type="date" id="start" name="trip-start" >
-											<input type="date" id="end" name="trip-start" >
+											<input type="date" id="start" name="start" >
+											<input type="date" id="end" name="end" >
 										</p>
 									</div>	
 								</div>
